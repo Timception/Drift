24e3c5da8b77fbf5eab700455302f12f92f6411f
Timothy Ryan <84595044+Timception@users.noreply.github.com>
2023-07-01 12:58:34 +0700

    Add files via upload

---
+++ drift/keymaps/luna/keymap.c
@@ -0,0 +1,77 @@
+#include QMK_KEYBOARD_H
+#include <stdio.h>
+#include "drift.h"
+
+// d e f i n e   l a y e r s
+
+enum Layers {
+    _QWERTY,
+    _LOWER,
+    _RAISE,
+    _ADJUST
+};
+
+enum custom_keycodes {
+    QWERTY,
+    LOWER,
+    RAISE,
+    ADJUST
+};
+
+#define QWERTY MO(_QWERTY)
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
+#define ADJUST MO(_ADJUST)
+
+
+// K E Y M A P 
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+ 
+   [_QWERTY] = LAYOUT(
+ 
+    KC_DEL,  KC_ESC,  KC_GRV,  KC_1, KC_2, KC_3,    KC_4,   KC_5,                    KC_6,  KC_7,   KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+    C(KC_C), ADJUST,  KC_TAB,  KC_Q, KC_W, KC_E,    KC_R,   KC_T,                    KC_Y,  KC_U,   KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+             KC_LGUI, KC_CAPS, KC_A, KC_S, KC_D,    KC_F,   KC_G,                    KC_H,  KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,
+    C(KC_V), KC_LCTL, KC_LALT, KC_Z, KC_X, KC_C,    KC_V,   KC_B,  KC_TRNS, KC_TRNS, KC_N,  KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_RALT, KC_RCTL, KC_DEL,
+	                                       KC_LSFT, KC_SPC, LOWER,                   RAISE, KC_SPC, KC_RSFT
+ ),
+
+
+   [_LOWER] = LAYOUT(
+
+    QK_BOOT, KC_ESC,  KC_GRV,  KC_1, KC_2, KC_3,    KC_4,   KC_5,                      KC_6,    KC_7,   KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  QK_BOOT,
+    C(KC_C), KC_TRNS, KC_TAB,  KC_Q, KC_W, KC_E,    KC_R,   KC_T,                      KC_Y,    KC_U,   KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+             KC_LGUI, KC_CAPS, KC_A, KC_S, KC_D,    KC_F,   KC_G,                      KC_H,    KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_UP,
+    C(KC_V), KC_LCTL, KC_LALT, KC_Z, KC_X, KC_C,    KC_V,   KC_B,    KC_TRNS, KC_TRNS, KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT,
+	                                       KC_LSFT, KC_SPC, KC_TRNS,                   KC_TRNS, KC_SPC, KC_RSFT
+ ),
+  
+
+   [_RAISE] = LAYOUT(
+ 
+    KC_DEL,  KC_ESC,  KC_GRV,  KC_1, KC_2, KC_3,    KC_4,   KC_5,                      KC_6,    KC_7,   KC_8,    KC_9,   KC_0,    KC_MINS, KC_WH_U, QK_BOOT,
+    C(KC_C), KC_TRNS, KC_TAB,  KC_Q, KC_W, KC_E,    KC_R,   KC_T,                      KC_Y,    KC_U,   KC_I,    KC_O,   KC_P,    KC_LBRC, KC_WH_D, KC_BTN2,
+             KC_LGUI, KC_CAPS, KC_A, KC_S, KC_D,    KC_F,   KC_G,                      KC_H,    KC_J,   KC_K,    KC_L,   KC_SCLN, KC_BTN1, KC_MS_U,
+    C(KC_V), KC_LCTL, KC_LALT, KC_Z, KC_X, KC_C,    KC_V,   KC_B,    KC_TRNS, KC_TRNS, KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_MS_L, KC_MS_D, KC_MS_R,
+	                                       KC_LSFT, KC_SPC, KC_TRNS,                   KC_TRNS, KC_SPC, KC_RSFT
+ ),
+ 
+
+   [_ADJUST] = LAYOUT(
+ 
+    KC_TRNS, KC_ESC,  KC_GRV,  KC_1, KC_2, KC_3,    KC_4,   KC_5,                      KC_6,    KC_7,   KC_8,    KC_9,   KC_0,    KC_MINS, KC_WH_U, KC_BSPC,
+    C(KC_C), KC_TRNS, KC_TAB,  KC_Q, KC_W, KC_E,    KC_R,   KC_T,                      KC_Y,    KC_U,   KC_I,    KC_O,   KC_P,    KC_LBRC, KC_WH_D, KC_BTN2,
+             KC_LGUI, KC_CAPS, KC_A, KC_S, KC_D,    KC_F,   KC_G,                      KC_H,    KC_J,   KC_K,    KC_L,   KC_SCLN, KC_BTN1, KC_MS_U,
+    C(KC_V), KC_LCTL, KC_LALT, KC_Z, KC_X, KC_C,    KC_V,   KC_B,    KC_TRNS, KC_TRNS, KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_MS_L, KC_MS_D, KC_MS_R,
+	                                       KC_LSFT, KC_SPC, KC_TRNS,                   KC_TRNS, KC_SPC, KC_RSFT
+ )
+};
+
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_WH_D, KC_WH_U) },
+    [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [2] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) },
+    [3] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS), ENCODER_CCW_CW(KC_TRNS, KC_TRNS) }
+};
---
+++ drift/keymaps/luna/oled/readme.md
@@ -0,0 +1,42 @@
+# OLED modules
+These modules are coded for Corne (CRKBD)'s 128x32 and can be integrated as standalone files without extra OLED code in `keymap.c`. They are setup to animate Bongocat or Luna on the primary display with status icons rendered on the secondary.
+
+# QMK Integration
+1. Save this folder in the user keymap (or userspace) directory.
+2. Add the following lines into `rules.mk`:
+   ```c
+   OLED_ENABLE = yes
+   SRC += oled/oled-bongocat.c oled/oled-icons.c
+   ```
+   (Substitute `oled-bongocat.c` with `oled-luna.c` for Luna the dog)
+   ```c
+   SRC += oled/oled-luna.c oled/oled-icons.c
+   ```
+3. Animation can be driven by WPM feature or input keystrokes:
+   * To animate with WPM, add `WPM_ENABLE = yes` into `rules.mk`.
+   * To animate with keystrokes, add the following 'process_record_user()' code block to the `keymap.c` file to trigger animation tap timer:
+   ```c
+   bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+       if (record->event.pressed) {
+           extern uint32_t oled_tap_timer;
+           oled_tap_timer = timer_read32();
+       }
+   }
+   ```
+4. Add the following directives into `config.h` to transmit state information to the secondary half for the status icons:
+   ```c
+   #define SPLIT_MODS_ENABLE
+   #define SPLIT_LED_STATE_ENABLE
+   #define SPLIT_LAYER_STATE_ENABLE
+   ```
+5. Add the following font file reference containing status icons into `config.h`:
+   ```c
+   #define OLED_FONT_H "oled/oledfont.c"
+   ```
+6. OLED code uses the following preprocessors as layer indicators. Add them into `config.h` and edit accordingly:
+   ```c
+   #define CMK 1
+   #define NUM 2
+   #define SYM 3
+   #define FNC 4
+   ```
