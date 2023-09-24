cd8690ca27511e6ed29770aba34d65de2bf60be0
Timothy Ryan <84595044+Timception@users.noreply.github.com>
2023-09-24 20:46:24 +0700

    Add files via upload

--- drift/V2_By_ZANNIKB/keymaps/vial/keymap.c
+++ drift/V2_By_ZANNIKB/keymaps/vial/keymap.c
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
    [_LOWER] = LAYOUT(
 
-    QK_BOOT, KC_ESC,  KC_GRV,  KC_F1,KC_F2,KC_F3,   KC_F4,  KC_F5,                     KC_F6,   KC_F8,  KC_F9,   KC_F10, KC_F11,  KC_F12,  KC_EQL,  QK_BOOT,
+    QK_BOOT, KC_ESC,  KC_GRV,  KC_F1,KC_F2,KC_F3,   KC_F4,  KC_F5,                     KC_F6,   KC_F8,  KC_F9,   KC_F10, KC_F11,  KC_F12,  KC_EQL,  KC_BSPC,
     C(KC_C), KC_TRNS, KC_TAB,  KC_Q, KC_W, KC_E,    KC_R,   KC_T,                      KC_Y,    KC_U,   KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
              KC_LGUI, KC_CAPS, KC_A, KC_S, KC_D,    KC_F,   KC_G,                      KC_H,    KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_UP,
     C(KC_V), KC_LCTL, KC_LALT, KC_Z, KC_X, KC_C,    KC_V,   KC_B,   MU_NEXT, KC_TRNS,  KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT,
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_DEL,  KC_ESC,  KC_GRV,  KC_F1,KC_F2,KC_F3,   KC_F4,  KC_F5,                     KC_F6,   KC_F8,  KC_F9,   KC_F10, KC_F11,  KC_F12,  KC_WH_U, QK_BOOT,
     C(KC_C), KC_TRNS, KC_TAB,  KC_Q, KC_W, KC_E,    KC_R,   KC_T,                      KC_Y,    KC_U,   KC_I,    KC_O,   KC_P,    KC_LBRC, KC_WH_D, KC_BTN2,
              KC_LGUI, KC_CAPS, KC_A, KC_S, KC_D,    KC_F,   KC_G,                      KC_H,    KC_J,   KC_K,    KC_L,   KC_SCLN, KC_BTN1, KC_MS_U,
-    C(KC_V), KC_LCTL, KC_LALT, KC_Z, KC_X, KC_C,    KC_V,   KC_B,    KC_TRNS, KC_TRNS, KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_MS_L, KC_MS_D, KC_MS_R,
+    C(KC_V), KC_LCTL, KC_LALT, KC_Z, KC_X, KC_C,    KC_V,   KC_B,    KC_TRNS, MU_TOGG, KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_MS_L, KC_MS_D, KC_MS_R,
 	                                       KC_LSFT, KC_SPC, KC_TRNS,                   KC_TRNS, KC_SPC, KC_RSFT
  ),
  
