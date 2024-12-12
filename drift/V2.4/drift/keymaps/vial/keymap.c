80bfb763e42eced9e576cbafac9fb2a9bc8fd7b5
Timception <timception@hotmail.com>
2024-12-12 13:46:48 +0700

    Update keymap.c

--- drift/V2.4/drift/keymaps/vial/keymap.c
+++ drift/V2.4/drift/keymaps/vial/keymap.c
@@ -77,11 +77,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  
    [_BASE] = LAYOUT(
  
-    KC_DEL,  KC_ESC,  KC_GRV,  KC_1, KC_2, KC_3,    KC_4,   KC_5,                    KC_6,  KC_7,   KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+    KC_ESC,  KC_ESC,  KC_GRV,  KC_1, KC_2, KC_3,    KC_4,   KC_5,                    KC_6,  KC_7,   KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
     C(KC_C), ADJUST,  KC_TAB,  KC_Q, KC_W, KC_E,    KC_R,   KC_T,                    KC_Y,  KC_U,   KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
              KC_LGUI, KC_CAPS, KC_A, KC_S, KC_D,    KC_F,   KC_G,                    KC_H,  KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,
     C(KC_V), KC_LCTL, KC_LALT, KC_Z, KC_X, KC_C,    KC_V,   KC_B, MU_TOGG, KC_MUTE,  KC_N,  KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_RALT, KC_RCTL, KC_DEL,
-	                                       KC_LSFT, KC_SPC, LOWER,                   RAISE, KC_SPC, KC_RSFT
+	                                       KC_LSFT, KC_SPC, LOWER,                   RAISE, KC_SPC, KC_BSPC
  ),
 
 
@@ -91,17 +91,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS, KC_TRNS, KC_TAB,  KC_Q, KC_W, KC_E,    KC_R,   KC_T,                      KC_Y,    KC_U,   KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
              KC_LGUI, KC_CAPS, KC_A, KC_S, KC_D,    KC_F,   KC_G,                      KC_H,    KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_UP,
     KC_TRNS, KC_LCTL, KC_LALT, KC_Z, KC_X, KC_C,    KC_V,   KC_B,   MU_NEXT, KC_TRNS,  KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT,
-	                                       KC_LSFT, KC_SPC, KC_TRNS,                   KC_TRNS, KC_SPC, KC_RSFT
+	                                       KC_LSFT, KC_SPC, KC_TRNS,                   KC_TRNS, KC_SPC, KC_BSPC
  ),
   
 
    [_RAISE] = LAYOUT(
  
-    KC_DEL,  KC_ESC,  KC_GRV,  KC_F1,KC_F2,KC_F3,   KC_F4,  KC_F5,                     KC_F6,   KC_F8,  KC_F9,   KC_F10, KC_F11,  KC_F12,  KC_WH_U, QK_BOOT,
+    KC_ESC,  KC_ESC,  KC_GRV,  KC_F1,KC_F2,KC_F3,   KC_F4,  KC_F5,                     KC_F6,   KC_F8,  KC_F9,   KC_F10, KC_F11,  KC_F12,  KC_WH_U, QK_BOOT,
     KC_TRNS, KC_TRNS, KC_TAB,  KC_Q, KC_W, KC_E,    KC_R,   KC_T,                      KC_Y,    KC_U,   KC_I,    KC_O,   KC_P,    KC_LBRC, KC_WH_D, KC_BTN2,
              KC_LGUI, KC_CAPS, KC_A, KC_S, KC_D,    KC_F,   KC_G,                      KC_H,    KC_J,   KC_K,    KC_L,   KC_SCLN, KC_BTN1, KC_MS_U,
     KC_TRNS, KC_LCTL, KC_LALT, KC_Z, KC_X, KC_C,    KC_V,   KC_B,    KC_TRNS, MU_TOGG, KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_MS_L, KC_MS_D, KC_MS_R,
-	                                       KC_LSFT, KC_SPC, KC_TRNS,                   KC_TRNS, KC_SPC, KC_RSFT
+	                                       KC_LSFT, KC_SPC, KC_TRNS,                   KC_TRNS, KC_SPC, KC_BSPC
  ),
  
 
