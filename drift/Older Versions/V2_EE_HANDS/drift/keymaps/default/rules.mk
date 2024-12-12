d8531e67de5cea14c13c07bac4baf22250f9258b
Timception <timception@hotmail.com>
2024-12-12 13:24:41 +0700

    File Organization

---
+++ drift/Older Versions/V2_EE_HANDS/drift/keymaps/default/rules.mk
@@ -0,0 +1,21 @@
+# VIA and VIAL Settings
+#VIA_ENABLE				= yes
+#VIAL_ENABLE			= yes
+#VIAL_INSECURE			= yes
+MOUSEKEY_ENABLE			= yes	# Mouse keys
+ENCODER_ENABLE			= yes
+ENCODER_MAP_ENABLE		= yes
+#VIAL_ENCODERS_ENABLE	= yes
+
+# Disabled Features (can be turned back on)
+CONSOLE_ENABLE			= no	# Console for debug
+COMMAND_ENABLE			= no	# Commands for debug and configuration
+QMK_SETTINGS			= no
+TAP_DANCE_ENABLE		= no
+COMBO_ENABLE			= no
+KEY_OVERRIDE_ENABLE		= no
+#VIALRGB_ENABLE			= no
+
+# OLED Animation Stuff
+#SRC					+= oled/oled-luna.c #oled/oled-icons.c
+#SRC					+= oled/oled-bongocat.c #oled/oled-icons.c
