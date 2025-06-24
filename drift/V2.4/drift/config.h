bfaa10ad055bafbead5ccdf99a6deca5e004aaeb
Timothy Ryan <timception@hotmail.com>
2025-06-24 14:24:57 +0700

    update

--- drift/V2.4/drift/config.h
+++ drift/V2.4/drift/config.h
@@ -1,63 +1,36 @@
-/* Copyright 2023 MAJIN <@timception>
+/* Copyright (c) 2025 Timception
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
+MIT License
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. */
 
 #pragma once
 
 #define DEBUG_MATRIX_SCAN_RATE
 
-//#include "config_common.h"
-
-/* USB Device descriptor parameter */
-/*
-#define VENDOR_ID       0x6D6A
-#define PRODUCT_ID      0x5965
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    MAJIN
-#define PRODUCT         DRIFT
-*/
-
-/* Key Matrix Size */
-// rows are doubled-up in split keyboards
-//#define MATRIX_COLS 8
-//#define MATRIX_ROWS 10
-//#define DIODE_DIRECTION COL2ROW
-
-/* Wiring of Columns and Rows */
-//#define MATRIX_COL_PINS { GP2, GP3, GP4, GP5, GP6, GP7, GP8, GP9 }		// 8 Pins
-//#define MATRIX_ROW_PINS { GP10, GP11, GP14, GP15, GP26 }				// 5 Pins
-
-/* -==== Encoder Pins ====- */
-//#define ENCODERS_PAD_A { GP28 }
-//#define ENCODERS_PAD_B { GP27 }
-//#define ENCODERS_PAD_A_RIGHT { GP27 }
-//#define ENCODERS_PAD_B_RIGHT { GP28 }
-
-//#define ENCODER_DIRECTION_FLIP
-//#define ENCODER_RESOLUTION 4
-//#define ENCODER_RESOLUTION 2
-//#define ENCODER_DEFAULT_POS 0x3
-
-/* Split Definitions */
-//#define SPLIT_LAYER_STATE_ENABLE
-//#define SPLIT_LED_STATE_ENABLE
-//#define SPLIT_MODS_ENABLE
 
 /* Serial settings */
 #define USE_SERIAL
 #define SOFT_SERIAL_PIN GP0 //TX in Schematic
 #define SERIAL_USE_MULTI_TRANSACTION
+
+
 /* Communication between Sides: The PIO driver */
 // The PIO subsystem is a Raspberry Pi RP2040 specific implementation,
 // using the integrated PIO peripheral and is therefore only available on this MCU.
@@ -67,24 +40,23 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 // The Half-duplex operation mode uses the built-in pull-ups and GPIO manipulation on the RP2040
 // to drive the line high by default. An external pull-up is therefore not necessary.
 
-//Flip the toggle switch up to MSTR (master state) when flashing each side individually (remove the 3.5mm cable before flashing firmware).
-//After done flashing each side, switch the slave half's toggle switch down to SLV (slave) before use.
+
+/* Handedness https://docs.qmk.fm/features/split_keyboard */
+// qmk flash -kb drift -km vial -bl uf2-split-left
+// qmk flash -kb drift -km vial -bl uf2-split-left
 //#define MASTER_LEFT
 //#define MASTER_RIGHT
 #define EE_HANDS
 
-/* External VBUS via 5V through diode to GP29 */
+
+/* Detection and Watchdog */
 // for rp2040 | When USB_VBUS_PIN is not defined, SPLIT_USB_DETECT is used.
 //#undef SPLIT_USB_DETECT
 #define SPLIT_USB_DETECT
+#define SPLIT_USB_TIMEOUT_POLL 10
 #define SPLIT_WATCHDOG_ENABLE
-//#define USB_VBUS_PIN GP29
+#define SPLIT_WATCHDOG_TIMEOUT 3000
 
-
-/* Fix for the RP2040-ZERO Wake up bug - you can't enter BIOS with these enabled tho */
-// use this instead of VBUS hack above if lazy to use diodes and switches on each side
-//#define SPLIT_USB_TIMEOUT 10000
-//#define SPLIT_USB_TIMEOUT_POLL 10
 
 /* Use 1000hz polling */
 #define USB_POLLING_INTERVAL_MS 1
