4cf53c3928eeb277d271dda6cf989ff848bd331b
Timothy Ryan <84595044+Timception@users.noreply.github.com>
2025-07-16 16:30:15 +0700

    Add files via upload

--- drift/V2.4/drift/keymaps/vial/rules.mk
+++ drift/V2.4/drift/keymaps/vial/rules.mk
@@ -10,7 +10,7 @@ ENCODER_MAP_ENABLE		= yes
 # Disabled Features (can be turned back on)
 CONSOLE_ENABLE			= no	# Console for debug
 COMMAND_ENABLE			= no	# Commands for debug and configuration
-QMK_SETTINGS			= no
+QMK_SETTINGS			= yes
 TAP_DANCE_ENABLE		= yes
 COMBO_ENABLE			= yes
 KEY_OVERRIDE_ENABLE		= yes
